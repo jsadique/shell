3d637342f33dd9598d8c61d23a5c48a8366fb901
jserver50 <jsadique33@gmail.com>
2020-11-16 12:02:38 +0530

    install nginx

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+
+case $USER_ID in
+  0)
+    echo "\e[1;31mYou should be a root user to perform this script\e[0m"
+    exit 1
+    ;;
+esac
+
 case $1 in
   frontend)
-    echo Installing frontend
-    echo Completed Installing frontend
+    echo -e "\e[1;31m*******>>>>>Installing Nginx<<<<<*******\e[0m"
+    yum install nginx -y
+    echo -e "\e[1;31m*******>>>>>Starting Nginx<<<<<********\e[0m"
+
     ;;
   catalogue)
     echo Installing
