a11c765c3e8a4640c50318a67b517024f06cc266
jserver50 <jsadique33@gmail.com>
2020-11-16 06:13:19 +0530

    case command

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo Installing Frontend
-echo Istalling Catalogue
-echo Installing Cart
+case $1 in
+  frontend)
+    echo Installing frontend
+    echo Completed Installing frontend
+    ;;
+  catalogue)
+    echo Installing
+    echo completed Installing catalogue
+    ;;
+  cart)
+    echo Installing cart
+    echo completed Installing cart
+    ;;
+esac
