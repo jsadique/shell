fdd35fc6aee253481ba4c23f4c847fe53d0b18bf
jserver50 <jsadique33@gmail.com>
2020-11-27 10:15:29 +0530

    mongodb installation schema

--- roboshop.sh
+++ roboshop.sh
@@ -78,7 +78,21 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
     systemctl enable mongod
     systemctl start mongod
     Status_Check
+    Print "Downloading Schema"
+    curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+    Status_Check
+    cd /tmp
+    Print "Extracting Archive"
+    unzip mongodb.zip
+    Status_Check
+    Print "Load Catalogue Schema"
+    mongo < catalogue.js
+    Status_Check
+    Print "Load User Schema"
+    mongo < users.js
+    Status_Check
     ;;
+
   *)
     echo "Invalid Input, Following Inputs are not acceptable"
     echo "Usage: $0 frontend|catalogue|cart|mongodb "
