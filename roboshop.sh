316a963e52a9ba6f0f360121fde16758b11200b6
jserver50 <jsadique33@gmail.com>
2020-12-02 11:59:31 +0530

    minor edit on whole.2

--- roboshop.sh
+++ roboshop.sh
@@ -57,10 +57,10 @@ esac
  chown roboshop:roboshop /home/roboshop -R
  Print "Setup $1 Service"
  Print " Set up catalogue service"
-  mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
-  sed -i -e "s/MONGO_ENDPOINT/mongodb.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
-  sed -i -e "s/REDIS_ENDPOINT/redis.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
-  sed -i -e "s/CATALOGUE_ENDPOINT/catalogue.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
+ mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+ sed -i -e "s/MONGO_ENDPOINT/mongodb.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
+ sed -i -e "s/REDIS_ENDPOINT/redis.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
+ sed -i -e "s/CATALOGUE_ENDPOINT/catalogue.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
 
   Status_Check
   Print "Start $1 Service"
